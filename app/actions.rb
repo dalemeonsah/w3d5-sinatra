dcba4d6bcb127ca955883943741a62a0c8c00ca5
Reinhard Simeon <reinhardsimeon@gmail.com>
2016-02-20 22:12:32 +0000

    finished part 7

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,28 @@
 get '/' do
   erb :index
 end
+
+get '/messages' do
+  @messages = Message.all
+  erb :'messages/index'
+end
+
+get '/messages/new' do
+  erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+post '/messages' do
+  binding.pry
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]  
+  )
+  @message.save
+  redirect '/messages'
+end
